a9e3d2b8b1c54ac6bbd0716e6f13578c9829940b
01 <ab40905045@gmail.com>
2026-03-10 12:04:32 +0900

    adsf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={poppins.variable}>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-F32JVK9158"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F32JVK9158');
+          `}
+        </Script>
+      </head>
       <body>
         <Script
           src="https://cdn.iamport.kr/v1/iamport.js"
